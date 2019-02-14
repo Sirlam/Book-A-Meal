9b6d907e73e96bc77b41f66596fa55b425ed5f50
Salam Olanrewaju <salam_lanre@yahoo.com>
2019-02-14 16:16:16 +0100

    Bootstrap a sample express app

---
+++ API/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 9001;
+
+
+app.get('/', (req, res) => res.send('The API is working'));
+
+app.listen(PORT, () => {
+  console.log(`APP is running on: ${PORT}`);
+});
