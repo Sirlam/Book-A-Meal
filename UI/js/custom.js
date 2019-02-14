0cf6de5b4bc20bed0ece8a22f4d2bec0a46cdfdc
Salam Olanrewaju <salam_lanre@yahoo.com>
2019-02-14 12:59:28 +0100

    Removed UI folder

--- UI/js/custom.js
+++
@@ -1,7 +0,0 @@
-function openLeftMenu(){
-    document.getElementById("leftMenu").style.display = "block";
-}
-
-function closeLeftMenu() {
-    document.getElementById("leftMenu").style.display = "none";
-}
